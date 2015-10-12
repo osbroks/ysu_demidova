9928a63624fedc0b1e060b6b51bf68d6dea11afc
osbroks <osbroks@gmail.com>
2015-10-12 23:12:08 +0300

    Update moda.c

--- moda.c
+++ moda.c
@@ -2,49 +2,39 @@
 
 #pragma warning(disable: 4996)
 
-#define N 3
+#define N 4
 system();
 
 int main(void) {
 
-	int A[N] = {0}, B[N] = {0};
+	int A[N];
 
 	int i = 0,
 		j = 0,
 		max = 0,
-		ner = 0,
 		moda = 0,
-		chastota = 1;
+		chastota = 0;
 
 	printf("Insert array A[], a number of elements = %d\n", N);
 
 	while (scanf("%d", &A[i++]) == 1 && i < N);
-	for (i = 0; i < N; i++) printf("%d\n", A[i]);
 
 	for (i = 0; i < N; i++) {
-		if (moda != A[i]) {
-			moda = A[i];
-			for (j = 1; j <= N; j++)
-				if (moda == A[j]) {
-					chastota++;
-					B[moda] = chastota;
-				}
+		chastota = 0;
+		for (j = 0; j < N; j++) {
+			if (A[i] == A[j]) {
+				chastota++;
+			}	
 		}
-		else { i++; }
+		if (chastota > moda) {
+			moda = chastota;
+		}
+		if (chastota == moda) {
+			max = A[i];
+		}
 	}
 
-	for (i = 0; i < N; i++) {
-		if (B[i] == B[i + 1])
-			ner++;
-	}
-
-	max = B[0];
-	for (i = 0; i < N; i++) {
-		if (B[i] > max)
-			max = B[i];
-	}
-
-	printf("Moda: %d", max);
+	printf("\nKolichestvo elementov = %d, Moda = %d\n", moda, max);
 
 	system("pause");
 
