334f194c1a42747ea970fa55cf5adc31a4fc2497
osbroks <osbroks@gmail.com>
2015-11-16 12:13:34 +0300

    Update larina3.c

--- larina3.c
+++ larina3.c
@@ -7,172 +7,123 @@
 состоящие из одинковых букв и содержащие не менее 2 символов.
 */
 #include<stdio.h>
+#include<stdlib.h>
 
-#pragma warning(disable: 4669)
+#define WORD 20
+#define WORDS 30
 
-#define WORD 20 // длина слова
-#define WORDS 30 //количество слов
-
-void CheckInsert(int, char*, char*);
-void CheckWords(int, char*);
-void CheckSentences(int, char*);
-void CheckSpaces(int, char*, char*);
-int LenCheckedSentences(int, char*);
-void PrintArray(int, char*);
-void FindWords(int, char*, char*);
-system();
-
-int main(int argv, char *argc[]) {
-	int	i = 0,
-		len = 0, //длина массива
-		ch = 0;
-	int lenSentences = 0;
-	char Sentences[WORD*WORDS];
-	char checkedSentences[WORD*WORDS];
-	char words[WORD*WORDS];
-
-	for (i = 0; i < WORD*WORDS; i++) {
-		checkedSentences[i] = '*';
-		words[i] = 0;
+int checkSentence(int, char *);
+void checkWord(int, char *);
+void checkWords(int, char *);
+void printWords(int, char *);
+int main(int argv, char *argc){
+	int i = 0,
+		len = 0,
+		lenSentence = 0,
+		flag = 0;
+	char sentence[WORD*WORDS];
+	char ch;
+	
+	while((ch = getchar()) != '\n'){
+		if(ch != ' '){
+			flag = 0;
+			sentence[i] = ch;
+			i++;
+			len++;
+		}
+		else if(flag == 0){
+			flag = 1;
+			sentence[i] = ch;
+			i++;
+			len++;
+		}
 	}
 
-	i = 0;
-	while ((ch = getchar()) != '\n') {
-		Sentences[i] = ch;
-		i++;
-		len++;
-	}
-
-	CheckInsert(len, Sentences, checkedSentences);
-	lenSentences = LenCheckedSentences(len, checkedSentences);
-
-	PrintArray(lenSentences, checkedSentences);
-
-	FindWords(lenSentences, checkedSentences, words);
+	lenSentence = checkSentence(len, sentence);
+	checkWord(lenSentence, sentence);
+	checkWords(lenSentence, sentence);
+	printWords(lenSentence, sentence);
 
 	system("PAUSE");
 	return 0;
 }
 
-void CheckInsert(int len, char* Sentences, char* checkedSentences) {
+void printWords(int lenSentence, char *sentence){
 	int i = 0,
-		lenSentences = 0;
-	CheckSpaces(len, Sentences, checkedSentences);
-	lenSentences = LenCheckedSentences(len, checkedSentences);
-	CheckWords(lenSentences, checkedSentences);
-	CheckSentences(lenSentences, checkedSentences);
-}
+		c = 0,
+		len = 0,
+		num = 0,
+		pointer = 0;
 
-int LenCheckedSentences(int len, char* checkedSentences) {
-	int i = 0,
-		lenSentences = 0;
-	for (i = 0; i < len; i++) {
-		if (checkedSentences[i] == '*') {
-			break;
+	char ch, ch1;
+	ch = sentence[0];
+	for(i = 0; i <= lenSentence; i++){
+		ch1 = sentence[i];
+		if(ch == ch1 && (ch1 != ' ' || ch1 != '\0')){
+			len++;
+		}
+		else if (len >= 2 && (ch1 != ' ' || ch1 != '\0')){
+			pointer = i - len;
+			num++;
+			printf("word %d: ", num);
+			for(c = pointer; c <u i; c++){
+				printf("%c", sentence[c]);
+			}
+			printf("\n");
+			len = 0;
+			ch = sentence[i+1];
 		}
 		else {
-			lenSentences++;
+			len = 0;
+			ch = sentence[i+1];
 		}
 	}
-	return lenSentences;
 }
 
-void PrintArray(int lenSentences, char* checkedSentences) {
-	int i = 0;
-	printf("Array: ");
-	for (i = 0; i < lenSentences; i++) {
-		printf("%c", checkedSentences[i]);
-	}
-	puts("\n");
-}
-
-void CheckSpaces(int len, char* Sentences, char* checkedSentences) {
+void checkWords(int lenSentence, char *sentence){
 	int i = 0,
-		num = 0,
-		lenSentences = 0,
-		flag = -1;
-	char ch;
-	for (i = 0; i < len; i++) {
-		ch = Sentences[i];
-		if (ch != ' ') {
-			if (checkedSentences[num] == ' ') {
-				num++;
-				checkedSentences[num] = ch;
-			}
-			checkedSentences[num] = ch;
-			flag = 0;
-			num++;
+		words = 0;
+	for(i = 0; i < lenSentence; i++){
+		if(sentence[i] == ' '){
+			words++;
 		}
-		else if (flag == 0) {
-			checkedSentences[num] = ch;
-			flag = 1;
+		if(words > WORDS){
+			printf("INCORRECT SENTENCE");
+			return;
 		}
 	}
 }
 
-void CheckWords(int len, char* checkedSentences) {
+void checkWord(int lenSentence, char *sentence){
 	int i = 0,
-		wordLen = 0,
-		flag = 0;
-	char ch;
-	for (i = 0; i < len; i++) {
-		ch = checkedSentences[i];
-		if (ch == ' ') {
+		wordLen = 0;
+	for(i = 0; i < lenSentence; i++){
+		if(sentence[i] == ' '){
 			wordLen = 0;
 		}
-		else {
+		else{
 			wordLen++;
 		}
-		if (wordLen > WORD) {
-			printf("Incorrect WORD, Word must been <= 20 chars\n");
+		if(wordLen > WORD){
+			printf("\nINCORRECT WORD");
 			return;
 		}
 	}
 }
 
-void CheckSentences(int lenSentences, char* checkedSentences) {
+int checkSentence(int len, char *sentence){
 	int i = 0,
-		num = 0,
-		word = 0;
-	for (i = 0; i < lenSentences; i++) {
-		if (checkedSentences[i] == ' ') {
-			word++;
-		}
-		if (word > WORDS) {
-			printf("Incorrect Sentences");
-			return;
-		}
-		if (checkedSentences[i] == '!' ||
-			checkedSentences[i] == '?' ||
-			checkedSentences[i] == '.') {
-			num++;
-			printf("Sentences %d\n", num);
-		}
-	}
-}
-
-void FindWords(int lenSentences, char* checkedSentences, char* words) {
-	int i = 0,
-		j = 0,
-		flag = 0,
-		len = 0;
-	char ch1, chI;
-	for (i = 0; i < lenSentences; i++) {
-		ch1 = checkedSentences[i];
-		chI = checkedSentences[i + 1];
-		if (ch1 == chI) {
-			ch1 = chI;
-			len++;
-			flag = 1;
-			words[i] = ch1;
+		lenSentence = 0;
+	for(i = 0; i < len; i++){
+		if(sentence[i] == '.' ||
+			sentence[i] == '!' ||
+			sentence[i] == '?'){
+				printf("\nEND SENTENCE");
+				break;
 		}
 		else {
-			flag = 0;
-			words[i] = 0;
-		}
-		if (checkedSentences[i] == ' ') {
-			len = 0;
-			words[i] = ' ';
+			lenSentence++;
 		}
 	}
+	return lenSentence;
 }
