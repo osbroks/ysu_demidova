eb42521da4a7a47d4cede24aa5b47b3219ad4867
osbroks <osbroks@gmail.com>
2017-04-30 13:23:32 +0300

    Update Bank.java

--- Bank/Bank.java
+++ Bank/Bank.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 /**
  * Created by hamas on 28.04.17.
 
@@ -22,6 +20,8 @@ import java.util.Scanner;
  ну размера вклада.
 
  */
+import java.util.Scanner;
+
 public class Bank {
     public static void main(String[] args) throws Exception {
         System.out.println("hi am bank");
@@ -35,25 +35,55 @@ public class Bank {
         int vid, summ;
         float gp;
         boolean adding;
-//
-//        Inner inner = new Inner(name, sourName, fatherName, vid, summ);
-//        Inner.Inn inn = inner.new Inn(gp, adding);
 
         choose = scn.nextInt();
         switch (choose){
             case 1: {
                 System.out.println("Добавить вкладчика и вклад");
+                System.out.println("Введите имя ");
                 name = scn.next();
+                System.out.println("Ведите фамилию");
                 sourName = scn.next();
+                System.out.println("Введите отчество");
                 fatherName = scn.next();
+                System.out.println("Вид вклада");
                 vid = scn.nextInt();
+                System.out.println("Сумма вклада");
                 summ = scn.nextInt();
+                System.out.println("Годовой процент");
                 gp = scn.nextFloat();
+                System.out.println("Возможность пополнения(true/false)");
                 adding = scn.nextBoolean();
                 Inner inner = new Inner(name, sourName, fatherName, vid, summ);
                 Inner.Inn inn = inner.new Inn(gp, adding);
                 innerScn.writeInnerToFile(inner);
                 innScn.writeInnToFile(inn);
+                break;
+            }
+            case 2: {
+                System.out.println("Удалить вклад и вкладчика");
+                int id = scn.nextInt();
+                innerScn.deleteInner(id);
+                innScn.deleteInn(id);
+                break;
+            }
+            case 3: {
+                System.out.println("Пополнить счет по ID");
+                int id = scn.nextInt();
+                summ = scn.nextInt();
+                innerScn.addSumm(id, summ);
+                break;
+            }
+            case 4: {
+                System.out.println("Снять проценты с вклада");
+                int id = scn.nextInt();
+                innerScn.getPercent(id);
+                break;
+            }
+            case 5: {
+                System.out.println("Вывод на экран списка вкладчиков, отсортированных по фамилии");
+                innerScn.sortBySourName();
+                break;
             }
         }
     }
